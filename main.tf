2f0823fb9dc0c2e30757534ada12314bd29aa831
Mustapha <elphil.mustapha.kamel@gmail.com>
2024-02-08 11:04:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "bucket" {
   bucket = "reply-devops-summit-2024" # Bucket names must be unique across all AWS users
   acl    = "private" # Defines who can access the bucket. Options include: private, public-read, public-read-write, aws-exec-read, authenticated-read, and log-delivery-write.
 }
