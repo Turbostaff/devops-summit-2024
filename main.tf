08bf4b98678db4139670be45890e44e0dadf8952
Mustapha <elphil.mustapha.kamel@gmail.com>
2024-03-29 13:43:04 +0100

    commit message

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1" # Replace with your desired AWS region
 }
 
 data "aws_ami" "amazon_linux" {
@@ -10,13 +10,18 @@ data "aws_ami" "amazon_linux" {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
-resource "aws_instance" "t2_micro" {
+resource "aws_instance" "ec2_instance" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
   tags = {
-    Name = "t2-micro-instance"
+    Name = "Amazon Linux Instance"
   }
 }
